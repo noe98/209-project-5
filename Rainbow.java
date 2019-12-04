b961c6835c92bcb9446431925a52c65807b719a6
ElyssaMcMaster <56412984+ElyssaMcMaster@users.noreply.github.com>
2019-12-04 17:07:46 -0500

    Rainbow Singleton
    making Rainbow a proper singleton by setting Rainbow to private

--- Rainbow.java
+++ Rainbow.java
@@ -13,10 +13,11 @@ import java.awt.Color;
 
 public class Rainbow{
     Color[] colors;
-    Color[] seeds = {Color.BLACK, Color.WHITE, Color.BLACK};
+    Color[] seeds = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA};
     int counter = 0;
     int limit;
     int colorSelect;
+    int index;
     static Rainbow inst;
     
 
@@ -25,9 +26,9 @@ public class Rainbow{
      * Rainbow
      * creates an array of colors and sets its max to the limit
      */
-    public Rainbow(int n){
-        this.limit = n;
-        this.colors = new Color[n];
+    private Rainbow(int n){
+        fillRainbowColors(n);
+        
     }
 
     /**
@@ -54,37 +55,29 @@ public class Rainbow{
      */
 
     public void fillRainbowColors(int limit){
+        this.colors = new Color[limit];
         double numSeeds = seeds.length;
-        double numSpots = (limit - 1)/(numSeeds -1);
+        double numSpots = (int)((limit - 1)/(numSeeds -1));
         for (int i = 0; i < limit; i++){
             Color startColor =  seeds[(int) (i / numSpots)];
             Color endColor = seeds[Math.min((int) (i / numSpots) + 1, (seeds.length -1))];
-            double percent = (i % ((int) numSpots)) / numSpots;
+            double percent = (i % ((int) numSpots)) / (numSpots);
             int r = (int) (percent * (endColor.getRed() - startColor.getRed()) +startColor.getRed());
             int b = (int) ((percent * (endColor.getBlue() - startColor.getBlue())) +startColor.getBlue());
             int g = (int) (percent * (endColor.getGreen() - startColor.getGreen()) +startColor.getGreen());
             colors[i] = new Color(r, b, g);
             System.out.println(colors[i]);
-            
+            System.out.println(percent);
+            System.out.println(startColor + ", " + endColor);     
         }
     }
 
 
+    // given an index, get the color at that index
+    public Color getColor(int index){
+        return colors[index];
+        }
 
-   // public void setEdgeCase(Rainbow[] colors, Color[] seeds){
-   //     colors[-1] = seeds [-1];
-   // }
-
-    /**
-     * removeColor
-     * removes colors from the counter
-     */
-
-    public void removeColor(){
-        if(counter > 0){
-            this.colors[counter] = null;
-            counter--;
-        }   
-    }
 }
+
 
--- RainbowTester.java
+++ RainbowTester.java
@@ -1,10 +1,13 @@
 //Tester file for Rainbow.java
 
 public class RainbowTester{
-    int n = 10;
+    int n = 32;
     public static void main(String[] args){
-        Rainbow one = new Rainbow(10);
-        one.fillRainbowColors(10);
+        Rainbow one = new Rainbow(33);
+        one.fillRainbowColors(33);
+        System.out.println("===============");
+        Rainbow two = new Rainbow(32);
+        two.fillRainbowColors(32);
 
     }
 }
