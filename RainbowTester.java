26e810c4370aa2577226e95649883df3f115ec5e
ElyssaMcMaster <56412984+ElyssaMcMaster@users.noreply.github.com>
2019-12-04 00:26:09 -0500

    Create RainbowTester.java

---
+++ RainbowTester.java
@@ -0,0 +1,10 @@
+//Tester file for Rainbow.java
+
+public class RainbowTester{
+    int n = 10;
+    public static void main(String[] args){
+        Rainbow one = new Rainbow(10);
+        one.fillRainbowColors(10);
+
+    }
+}
