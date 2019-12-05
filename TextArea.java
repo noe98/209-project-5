5816b986d36de6c66df57aa83a7d0fda1600b2d3
ElyssaMcMaster <56412984+ElyssaMcMaster@users.noreply.github.com>
2019-12-04 23:17:10 -0500

    TextArea.java
    First round of edits to TextArea.java; adding in calculations for x and y coordinates later.

---
+++ TextArea.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TextArea extends JPanel {
+    public TextArea() {
+        double xPos = 0;
+        double yPos = 0;
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        this.setLayout(new BorderLayout());
+        this.setPreferredSize(new Dimension(500, 200));
+
+        JTextArea textArea = new JTextArea(5, 50);
+        textArea.setText("X Coordinate: " + "\n" +"Y Coordinate: ");
+
+        // By default the JTextArea is editable, calling
+        // setEditable(false) produce uneditable JTextArea.
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public static void showFrame() {
+        JPanel panel = new TextArea();
+        panel.setOpaque(true);
+
+        JFrame frame = new JFrame("X and Y Coordinates ");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                TextArea.showFrame();
+            }
+        });
+    }
+}
